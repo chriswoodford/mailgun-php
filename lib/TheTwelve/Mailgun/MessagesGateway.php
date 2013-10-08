df3fd018eecb17482a3829cdf0f7cf23ca74babe
Chris Woodford <chris.woodford@gmail.com>
2013-10-08 10:05:53 -0400

    update messages gateway to manage mailgun response text

--- lib/TheTwelve/Mailgun/MessagesGateway.php
+++ lib/TheTwelve/Mailgun/MessagesGateway.php
@@ -2,12 +2,12 @@
 
 namespace TheTwelve\Mailgun;
 
-use Buzz\Exception\RuntimeException;
-
 use TheTwelve\Mailgun\Message;
 
 class MessagesGateway extends EndpointGateway
 {
+
+    const EMAIL_QUEUED_SUCCESS_MESSAGE = 'Queued. Thank you.';
 
     /**
      * create a new email message
@@ -21,8 +21,12 @@ class MessagesGateway extends EndpointGateway
     }
 
     /**
-     * send an email
+     * send an email. throws an exception if a successful connection
+     * is not made with mailgun. returns true if the email is queued
+     * successfully. false otherwise.
      * @param Message\EmailMessage $message
+     * @return boolean
+     * @throws \RuntimeException
      */
     public function send(Message\EmailMessage $message)
     {
@@ -30,11 +34,15 @@ class MessagesGateway extends EndpointGateway
         $params = $message->toArray();
         $response = $this->makeApiRequest('messages', $params, HttpClient::POST);
 
-        if ($response instanceof \stdClass) {
-            return $response->message;
+        if (!$response instanceof \stdClass) {
+            throw new \RuntimeException('Unable to queue message via mailgun');
         }
 
-        throw new \RuntimeException('Unable to queue message via mailgun');
+        if ($response->message != self::EMAIL_QUEUED_SUCCESS_MESSAGE) {
+            return false;
+        }
+
+        return true;
 
     }
 
